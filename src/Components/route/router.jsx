164e6e4585643fa3785759fdeac48b4a640441d7
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-01-05 15:50:11 +0600

    navbar section updateted

--- src/Components/route/router.jsx
+++ src/Components/route/router.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
             element: <Blog/>,
         },
         {
-            path: '/Contact',
+            path: '/contact',
             element: <Contact/>,
         },
         {
