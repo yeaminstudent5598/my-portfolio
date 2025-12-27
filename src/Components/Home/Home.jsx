a8cb50af72e58ab68a218338b1af38413d9242d2
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-12-27 11:59:10 +0600

    Update: src/Components/Home/Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import Banner from './Banner';
 import AboutMe from './AboutMe/AboutMe';
 import Skills from './Skill/Skills';
-import Experience from './Experience/Experience';
 import Education from './Education/Education';
 import Projects from './Projects/Projects';
-import Experienc from './Experienc/Experienc';
 import AnimationSection from './AnimationSection';
 
 const Home = () => {
@@ -16,7 +14,6 @@ const Home = () => {
             {/* <AnimationSection/> */}
             {/* <Experience/> */}
             <Skills/>
-            <Experienc/>
             <Education/>
             <Projects/>
         </div>
